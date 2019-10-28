21cf45193acbe02a750a1ca21f2b2e8067d428b3
Peter Piliar <34344330+ppiliar@users.noreply.github.com>
2019-10-28 20:38:36 +0100

    Update zmaz-osobu.php

--- zmaz-osobu.php
+++ zmaz-osobu.php
@@ -5,14 +5,12 @@
 //mysql_select_db ("zaklad") or die ("nepodarilo sa otvorit databazu");
  
 $con=mysqli_connect($config->host,$config->username,$config->password,$config->database) or die ("conect error");
-$con1=mysqli_connect($config->host1,$config->username1,$config->password1,$config->database1) or die ("conect error");
 
 $pressed = $_POST["pressed"];
 
 $sql = "DELETE FROM osoba WHERE rc='$pressed'";
 
 $result = mysqli_query($con, $sql) or die ("chybny dotaz");
-$result1 = mysqli_query($con1, $sql) or die ("chybny dotaz");
 header("Location: index.php?menu=3");
 die();
 ?>
