239888d09a2842937c20bb2f78b2f1b7ffe1a312
Peter Piliar <34344330+ppiliar@users.noreply.github.com>
2019-10-28 20:37:16 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,12 +3,6 @@
         'host' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'dsd1',
-		
-		'host1' => '192.168.56.101',
-		'username1' => 'root',
-        'password1' => '',
-        'database1' => 'dsd1',
-		
+        'database' => 'dsd1'
     );
 ?>
